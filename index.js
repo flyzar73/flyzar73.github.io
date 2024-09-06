0a51d78571e706666a91380b1dd49298eaf0f7ea
flyzar73 <61349213+flyzar73@users.noreply.github.com>
2024-09-06 23:15:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const { DiscordSDK } = require("@discord/embedded-app-sdk");
+
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: ["identify", "guilds", "applications.commands"],
+  });
+
+  // Retrieve an access_token from your activity's server
+  // Note: We need to prefix our backend `/api/token` route with `/.proxy` to stay compliant with the CSP.
+  // Read more about constructing a full URL and using external resources at
+  // https://discord.com/developers/docs/activities/development-guides#construct-a-full-url
+  const response = await fetch("http://192.168.1.10:10001/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+}
+
 document.querySelector("#app").innerHTML = `
   <div>
     <img src="./rocket.png" class="logo" alt="Discord" />
